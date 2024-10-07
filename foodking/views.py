471d4f370f3f67f01d9463370a4e89bfd4cea7a8
bainaryho <rkswl233@naver.com>
2024-10-07 19:36:52 +0900

    feat : add DRF api FoodViewSet

--- foodking/views.py
+++ foodking/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Food
+from .serializers import FoodSerializer
 
-# Create your views here.
+# 기존 HTML 뷰
 def index(request):
     return render(request, 'foodking/index.html')
+
+# DRF ViewSet
+class FoodViewSet(viewsets.ViewSet):
+    queryset = Food.objects.all()  # queryset 속성 추가 ViewSet을 사용할 때는 일반적으로 queryset 속성정의
+    def list(self, request):
+        queryset = Food.objects.all()
+        serializer = FoodSerializer(queryset, many=True)
+        return Response(serializer.data)
